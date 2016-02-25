08c4b7b8ee7db28da296ae37aec7e2d6e2601417
Tamal Kanti Nath <Tamal.Nath@gmail.com>
2016-02-25 16:35:27 +0530

    Added logging

--- api/webserver.js
+++ api/webserver.js
@@ -8,8 +8,32 @@ const mimeTypes = {
 	'.png'   : 'image/png',
 	'.jpg'   : 'image/jpg'
 };
+const colors = {
+	regular : {
+		black  : '\x1b[30;2m',
+		red    : '\x1b[31;2m',
+		green  : '\x1b[32;2m',
+		yellow : '\x1b[33;2m',
+		blue   : '\x1b[34;2m',
+		magenta: '\x1b[35;2m',
+		cyan   : '\x1b[36;2m',
+		white  : '\x1b[37;2m'
+	},
+	bold : {
+		black  : '\x1b[30;1m',
+		red    : '\x1b[31;1m',
+		green  : '\x1b[32;1m',
+		yellow : '\x1b[33;1m',
+		blue   : '\x1b[34;1m',
+		magenta: '\x1b[35;1m',
+		cyan   : '\x1b[36;1m',
+		white  : '\x1b[37;1m'
+	},
+	reset : '\x1b[0m'
+};
 const port = process.argv[2] || 8080;
 const base = process.argv[3] || process.cwd();
+process.title = "Web Server";
 http.createServer(createServer).listen(port);
 console.log('Serving ' + base + ' at http://127.0.0.1:' + port + '/');
 
@@ -27,6 +51,7 @@ function createServer (request, response) {
 				response.end('500 : ' + fsPath + ' returns error: ' + err.code);
 				console.log(err);
 			}
+			log(response.statusCode, fsPath);
 			return;
 		}
 		if (stats.isDirectory()) {
@@ -35,7 +60,9 @@ function createServer (request, response) {
 			}
 			fsPath += 'index.html';
 			fs.stat(base + fsPath, stat);
-		} else if (stats.isFile()) {
+			return;
+		}
+		if (stats.isFile()) {
 			var headers = {
 				'Content-Length' : stats.size,
 				'Last-Modified' : stats.mtime.toUTCString(),
@@ -67,5 +94,10 @@ function createServer (request, response) {
 			response.writeHead(403);
 			response.end('403 : ' + fsPath + ' Not a regular file');
 		}
+		log(response.statusCode, fsPath);
 	}
+}
+
+function log(statusCode, fsPath) {
+	console.log(colors.regular.magenta + new Date().toUTCString() + colors.regular.yellow + ' [' + statusCode + '] ' + colors.regular.white + fsPath + colors.reset);
 }
